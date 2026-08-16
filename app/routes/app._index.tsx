a41054870d5da19bf2540b936e5ae5185adce016
max23468 <30387529+max23468@users.noreply.github.com>
2026-08-16 13:52:10 +0200

    fix: isola il server della Home dal client

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,4 +1,20 @@
-export { action, headers, loader, shouldRevalidate } from "../features/home/home.server";
+import type { ActionFunctionArgs, HeadersFunction, LoaderFunctionArgs } from "react-router";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { skipRevalidationWhenLeaving } from "../revalidation";
+
+export const loader = async (args: LoaderFunctionArgs) => {
+  const server = await import("../features/home/home.server");
+  return server.loader(args);
+};
+
+export const action = async (args: ActionFunctionArgs) => {
+  const server = await import("../features/home/home.server");
+  return server.action(args);
+};
+
+export const headers: HeadersFunction = (args) => boundary.headers(args);
+export const shouldRevalidate = skipRevalidationWhenLeaving;
+
 export { PlanChoice } from "../features/home/PlanChoice";
 export { SetupGuide } from "../features/home/SetupGuide";
 export { default } from "../features/home/HomePage";
